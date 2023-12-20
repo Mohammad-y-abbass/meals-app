3843125c39fa9913b47214845e921babadf5844b
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2023-12-20 14:30:46 +0200

    added sqlite database

--- components/meals/MealsGrid.jsx
+++ components/meals/MealsGrid.jsx
@@ -1,12 +1,14 @@
 import MealItem from "./mealItem";
 import classes from "./mealsGrid.module.css";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsGrid({ meals }) {
+export default async function MealsGrid() {
+  const meals = await getMeals();
   return (
     <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
-          <MealItem {...meal} />
+          <MealItem meals={meals} />
         </li>
       ))}
     </ul>
